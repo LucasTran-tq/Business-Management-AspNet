2be4290fab4ce126e638186ad79ba26bab7ab292
Lucas Tran <lucastrantq@gmail.com>
2021-12-04 22:33:44 +0700

    initial Count Salary Function

--- Migrations/20211204030749_UpdateSalary.Designer.cs
+++ Migrations/20211204030749_UpdateSalary.Designer.cs
@@ -4,14 +4,16 @@ using App.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AppMvc.Net.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211204030749_UpdateSalary")]
+    partial class UpdateSalary
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -355,6 +357,9 @@ namespace AppMvc.Net.Migrations
                         .HasColumnType("int");
 
                     b.Property<int>("BasicSalaryId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("BonusLevel")
                         .HasColumnType("int");
 
                     b.Property<int>("BonusSalaryId")
